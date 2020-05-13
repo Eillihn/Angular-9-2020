f9c14e1dcaa172318c94d264619bd2ff6f1712d4
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-05-13 16:10:35 +0300

    task1-comments

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,6 +10,11 @@ import { CartService } from 'src/app/core/services/cart.service';
 export class ProductComponent implements OnInit {
     @Input() product: ProductModel;
 
+    // Не нужно внедрять тут сервис.
+    // Этот компонент получил данные через инпут от родителя,
+    // Пусть скажет родителю, что надо товар купить и скажет какой товар и сколько.
+    // Таким образом у родителя уже есть зависимость и если их будет не одна, а две ничего не измениться
+    // А у этого компонента зависимости не будет и это упростит компонент.
     constructor(public cartService: CartService) {
     }
 
@@ -17,7 +22,7 @@ export class ProductComponent implements OnInit {
     }
 
     onBuy(id: number) {
-        this.product.isAvailable = false;
+        this.product.isAvailable = false; // у вас будет доступно только 1 шт товара?
         this.cartService.addProduct(this.product);
         console.log('Buy ' + id);
     }
